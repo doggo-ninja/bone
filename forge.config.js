56a169c999f6795e2b8cca2bff8b557a7be797bc
Felix <felix.mattick@gmail.com>
2020-05-22 03:01:42 -0500

    Publishers

--- forge.config.js
+++ forge.config.js
@@ -25,5 +25,17 @@ module.exports = {
                 ]
             }
         }
+    ],
+    publishers: [
+        {
+            name: '@electron-forge/publisher-github',
+            config: {
+                repository: {
+                    owner: 'me',
+                    name: 'awesome-thing'
+                },
+                prerelease: true
+            }
+        }
     ]
 }
