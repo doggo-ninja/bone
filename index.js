3f364c3ba51b567afe274c45ef6502a0d41f7c1e
Andrew <ajkachnic@protonmail.com>
2020-06-29 20:57:51 -0400

    Fixed weird IP issue
    For some reason, the IP address of the server was being used instead of the domain. This fix also somehow brings linux support!

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const upload = async (path, update = () => {}) => {
     })
     prog.on('progress', update)
 
-    const res = await fetch(`http://144.202.58.157/v1/upload?originalName=${encodeURIComponent(pathLib.basename(path))}`, {
+    const res = await fetch(`https://pat.doggo.ninja/v1/upload?originalName=${encodeURIComponent(pathLib.basename(path))}`, {
         method: 'POST',
         headers: {
             'Authorization': `Bearer ${store.get('token')}`,
